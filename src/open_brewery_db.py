82df92dcd2ec6caf1ebdf81823491e5a729a1212
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 13:39:40 +0300

    Update open_brewery_db.py

--- src/open_brewery_db.py
+++ src/open_brewery_db.py
@@ -1,85 +1,75 @@
-from requests import get
+from requests import Session
 
-class OpenBreweryDB:
+class OpenBreweryDb:
 	def __init__(self) -> None:
 		self.api = "https://api.openbrewerydb.org"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 		}
 	
 	def get_single_brewery(self, brewery_id: str) -> dict:
-		return get(
-			f"{self.api}/breweries/{brewery_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries/{brewery_id}").json()
 	
 	def get_breweries_list(
 			self,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?page={page}&per_page={limit}").json()
 	
 	def get_breweries_by_city(
 			self,
 			city: str,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?by_city={city}&page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?by_city={city}&page={page}&per_page={limit}").json()
 	
 	def get_breweries_by_dist(
 			self,
 			dist: str,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?by_dist={dist}&page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?by_dist={dist}&page={page}&per_page={limit}").json()
 			
 	def get_breweries_by_name(
 			self,
 			name: str,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?by_name={name}&page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?by_name={name}&page={page}&per_page={limit}").json()
 	
 	def get_breweries_by_state(
 			self,
 			state: str,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?by_state={state}&page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?by_state={state}&page={page}&per_page={limit}").json()
 	
 	def get_breweries_by_postal(
 			self,
 			postal: int,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?by_postal={postal}&page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?by_postal={postal}&page={page}&per_page={limit}").json()
 	
 	def get_breweries_by_type(
 			self,
 			type: str,
 			page: int,
 			limit: int) -> dict:
-		return get(
-			f"{self.api}/breweries?by_type={type}&page={page}&per_page={limit}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries?by_type={type}&page={page}&per_page={limit}").json()
 	
 	def get_random_brewery(self) -> dict:
-		return get(
-			f"{self.api}/breweries/random",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/breweries/random").json()
 	
 	def search_breweries(self, query: str) -> dict:
-		return get(
-			f"{self.api}/breweries/search?query={query}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/breweries/search?query={query}").json()
